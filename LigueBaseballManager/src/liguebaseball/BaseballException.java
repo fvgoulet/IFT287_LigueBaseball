8dedcabcc275a236752ea69fcab67a11bbe7d90e
Tiger <Tiger@SS-Laptop>
2015-03-26 00:13:14 -0400

    Ajout MatchHandler/match

---
+++ LigueBaseballManager/src/liguebaseball/BaseballException.java
@@ -0,0 +1,15 @@
+package liguebaseball;
+
+
+public final class BaseballException extends Exception
+{
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public BaseballException(String message)
+{
+super(message);
+}
+}
