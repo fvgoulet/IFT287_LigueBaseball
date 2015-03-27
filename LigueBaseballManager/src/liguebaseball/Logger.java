f0916b5efe1af17f71d9c01b688457b14fc790ab
Félix Vallières Goulet <flx890@gmail.com>
2015-03-27 02:25:12 -0400

    Improved Logger

--- LigueBaseballManager/src/liguebaseball/Logger.java
+++ LigueBaseballManager/src/liguebaseball/Logger.java
@@ -20,6 +20,7 @@ import java.util.Date;
 public class Logger 
 {   
     private final BufferedWriter writer;
+    private static final String newLine = "\r\n";
     
     /**
      * Parametric Constructor
@@ -51,6 +52,16 @@ public class Logger
     }
     
     /**
+     * Method that output value to the console and write to the log file
+     * @param value What to Log
+     */
+    public void LogTimeStamped(String value) throws IOException
+    {
+        System.out.println(value);
+        write(value, getTimeStamp());
+    }
+    
+    /**
      * Method that Write all lines in an ArrayList of String into the Log file
      * @param lines All lines to write to the file as an ArrayList of String
      * @throws FileNotFoundException In case the file
@@ -75,7 +86,55 @@ public class Logger
     public void write(String content) throws FileNotFoundException, IOException
     {
         writer.write(content);
+        writer.newLine();
         writer.flush();
+    }
+    
+    /**
+     * Method that Write all TimeStamped lines in an ArrayList of String into the Log file
+     * @param lines All lines to write to the file as an ArrayList of String
+     * @param timestamp A timestamp to put before the logging value
+     * @throws FileNotFoundException In case the file
+     * @throws IOException In case the file couldn't be written in
+     */
+    public void write(ArrayList<String> lines, String timestamp) throws FileNotFoundException, IOException
+    {
+        for(String line : lines)
+        {
+            writer.write(timestamp + "   " + line);
+            writer.newLine();
+            writer.flush();
+        }
+    }
+    
+    /**
+     * Method that Write a TimeStamped String into the Log file
+     * @param content The String to write to the file
+     * @param timestamp A timestamp to put before the logging value
+     * @throws FileNotFoundException In case the file
+     * @throws IOException In case the file couldn't be written in
+     */
+    public void write(String content, String timestamp) throws FileNotFoundException, IOException
+    {
+        writer.write(timestamp + "   " + content);
+        writer.newLine();
+        writer.flush();
+    }
+    
+    /**
+     * Get a String representation of current time in a TimeStamp
+     * @return A timestamp as a String
+     */
+    private static String getTimeStamp()
+    {
+        Date date = new java.util.Date();
+        String dateTimeString = "[" + Integer.toString(date.getYear()) + "-";
+        dateTimeString += Integer.toString(date.getMonth()) + "-";
+        dateTimeString += Integer.toString(date.getDay()) + " ";
+        dateTimeString += Integer.toString(date.getHours()) + ":";
+        dateTimeString += Integer.toString(date.getMinutes())+ ":";
+        dateTimeString += Integer.toString(date.getSeconds()) + "]";
+        return dateTimeString;
     }
 }
 
